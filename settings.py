722007456af2f76c82da51e90da7ae681cb3b6cd
William Chen <chenw1511@gmail.com>
2024-04-26 00:55:40 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,11 +2,14 @@
 WIDTH, HEIGHT = 720, 530
 FPS = 60
 
-# paddle dimensions
+# color
+COLOR = "white"
+
+# paddle attributes
 PADDLE_WIDTH, PADDLE_HEIGHT = 10, 50
 PADDLE_SPEED = 10
 
-# ball dimensions
+# ball attributes
 BALL_WIDTH, BALL_HEIGHT = 10, 10
 BALL_SPEED = 5
 
