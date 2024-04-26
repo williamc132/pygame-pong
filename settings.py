858ab11b780c7cb3a6e903c9406993c1298e7ead
William Chen <chenw1511@gmail.com>
2024-04-26 00:52:57 -0400

    Revert "Add color variable"
    This reverts commit ae7e91b159c0a488c078c02a7f9fc27ff50e4f94.

--- settings.py
+++ settings.py
@@ -2,14 +2,11 @@
 WIDTH, HEIGHT = 720, 530
 FPS = 60
 
-# color
-COLOR = "white"
-
-# paddle attributes
+# paddle dimensions
 PADDLE_WIDTH, PADDLE_HEIGHT = 10, 50
 PADDLE_SPEED = 10
 
-# ball attributes
+# ball dimensions
 BALL_WIDTH, BALL_HEIGHT = 10, 10
 BALL_SPEED = 5
 
