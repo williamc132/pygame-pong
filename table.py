729b5ac798ea03e45c7d0c2d707ffb488088bc5f
William Chen <chenw1511@gmail.com>
2024-05-19 22:01:33 -0400

    Add new angles to paddle collision

--- table.py
+++ table.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 from settings import *
 from player import Paddle
 from ball import Ball
@@ -16,10 +17,12 @@ class Table:
         self.game_over = False
         self.score_limit = 11
         self.winner = None
-        self.score_font = pygame.font.Font(SCORE_FONT_NAME, SCORE_FONT_SIZE)
+        self.score_font = pygame.font.Font(SCORE_FONT, FONT_SIZE)
+        self.text_font = pygame.font.Font(TEXT_FONT, FONT_SIZE)
         self.color = pygame.Color(COLOR)
         self.score_time = pygame.time.get_ticks() - 2000
         self.RESET_BALL = pygame.USEREVENT + 1
+        self.paddle_collision_count = 0
 
     # draw net onto table object
     def _draw_net(self):
@@ -30,6 +33,7 @@ class Table:
 
     # handle paddle movement
     def _move_paddle(self):
+        # get state of all keyboard buttons
         keys = pygame.key.get_pressed()
 
         # handle left paddle movement
@@ -64,11 +68,13 @@ class Table:
             self.p2.score += 1
             self.score_time = pygame.time.get_ticks()
             self._hide_ball()
+            self._reset_ball_speed()
             self._check_score()
         if self.ball.rect.left >= WIDTH and pygame.time.get_ticks() - self.score_time > RESET_DELAY:
             self.p1.score += 1
             self.score_time = pygame.time.get_ticks()
             self._hide_ball()
+            self._reset_ball_speed()
             self._check_score()
 
     # collision between ball and paddles
@@ -79,17 +85,110 @@ class Table:
     # handle collision between ball and paddles
     def _handle_paddle_collision(self, ball, paddle, ball_direction):
         if self.ball.rect.colliderect(paddle):
-            # collision with each side of paddle
+            # collision with right paddle
             if abs(ball.rect.right - paddle.rect.left) < COLLISION_TOLERANCE and ball_direction > 0:
-                ball.speed_x *= -1
+                if abs(ball.rect.centery - paddle.rect.centery) < 1 and not self.game_over:
+                    self._check_ball_speed()
+                    ball.speed_x = self.ball.new_speed * math.cos(math.radians(180))
+                    ball.speed_y = self.ball.new_speed * math.sin(math.radians(180))
+
+                elif 0 < ball.rect.centery - paddle.rect.centery <= 5 and not self.game_over:
+                    self._check_ball_speed()
+                    ball.speed_x = self.ball.new_speed * math.cos(math.radians(160))
+                    ball.speed_y = self.ball.new_speed * math.sin(math.radians(160))
+
+                elif 5 < ball.rect.centery - paddle.rect.centery <= 10 and not self.game_over:
+                    self._check_ball_speed()
+                    ball.speed_x = self.ball.new_speed * math.cos(math.radians(140))
+                    ball.speed_y = self.ball.new_speed * math.sin(math.radians(140))
+
+                elif 10 < ball.rect.centery - paddle.rect.centery <= 15 and not self.game_over:
+                    self._check_ball_speed()
+                    ball.speed_x = self.ball.new_speed * math.cos(math.radians(120))
+                    ball.speed_y = self.ball.new_speed * math.sin(math.radians(120))
+
+                elif 0 > ball.rect.centery - paddle.rect.centery >= -5 and not self.game_over:
+                    self._check_ball_speed()
+                    ball.speed_x = self.ball.new_speed * math.cos(math.radians(200))
+                    ball.speed_y = self.ball.new_speed * math.sin(math.radians(200))
+
+                elif -5 > ball.rect.centery - paddle.rect.centery >= -10 and not self.game_over:
+                    self._check_ball_speed()
+                    ball.speed_x = self.ball.new_speed * math.cos(math.radians(220))
+                    ball.speed_y = self.ball.new_speed * math.sin(math.radians(220))
+
+                elif -10 > ball.rect.centery - paddle.rect.centery >= -15 and not self.game_over:
+                    self._check_ball_speed()
+                    ball.speed_x = self.ball.new_speed * math.cos(math.radians(240))
+                    ball.speed_y = self.ball.new_speed * math.sin(math.radians(240))
+
+                else:
+                    if not self.game_over:
+                        self._check_ball_speed()
+                    ball.speed_x *= -1
+
+            # collision with left paddle
             elif abs(ball.rect.left - paddle.rect.right) < COLLISION_TOLERANCE and ball_direction < 0:
-                ball.speed_x *= -1
+                if abs(ball.rect.centery - paddle.rect.centery) < 1 and not self.game_over:
+                    self._check_ball_speed()
+                    ball.speed_x = self.ball.new_speed * math.cos(math.radians(0))
+                    ball.speed_y = self.ball.new_speed * math.sin(math.radians(0))
+
+                elif 0 < ball.rect.centery - paddle.rect.centery <= 5 and not self.game_over:
+                    self._check_ball_speed()
+                    ball.speed_x = self.ball.new_speed * math.cos(math.radians(20))
+                    ball.speed_y = self.ball.new_speed * math.sin(math.radians(20))
+
+                elif 5 < ball.rect.centery - paddle.rect.centery <= 10 and not self.game_over:
+                    self._check_ball_speed()
+                    ball.speed_x = self.ball.new_speed * math.cos(math.radians(40))
+                    ball.speed_y = self.ball.new_speed * math.sin(math.radians(40))
+
+                elif 10 < ball.rect.centery - paddle.rect.centery <= 15 and not self.game_over:
+                    self._check_ball_speed()
+                    ball.speed_x = self.ball.new_speed * math.cos(math.radians(60))
+                    ball.speed_y = self.ball.new_speed * math.sin(math.radians(60))
+
+                elif 0 > ball.rect.centery - paddle.rect.centery >= -5 and not self.game_over:
+                    self._check_ball_speed()
+                    ball.speed_x = self.ball.new_speed * math.cos(math.radians(340))
+                    ball.speed_y = self.ball.new_speed * math.sin(math.radians(340))
+
+                elif -5 > ball.rect.centery - paddle.rect.centery >= -10 and not self.game_over:
+                    self._check_ball_speed()
+                    ball.speed_x = self.ball.new_speed * math.cos(math.radians(320))
+                    ball.speed_y = self.ball.new_speed * math.sin(math.radians(320))
+
+                elif -10 > ball.rect.centery - paddle.rect.centery >= -15 and not self.game_over:
+                    self._check_ball_speed()
+                    ball.speed_x = self.ball.new_speed * math.cos(math.radians(300))
+                    ball.speed_y = self.ball.new_speed * math.sin(math.radians(300))
+
+                else:
+                    if not self.game_over:
+                        self._check_ball_speed()
+                    ball.speed_x *= -1
+
+            # collision between top of paddle and bottom of ball
             elif abs(ball.rect.bottom - paddle.rect.top) < COLLISION_TOLERANCE and self.ball.speed_y > 0:
                 ball.speed_y *= -1
+                ball.rect.bottom = paddle.rect.top
+
+            # collision between top of ball and bottom of paddle
             elif abs(ball.rect.top - paddle.rect.bottom) < COLLISION_TOLERANCE and self.ball.speed_y < 0:
                 ball.speed_y *= -1
+                ball.rect.top = paddle.rect.bottom
 
-    # reposition ball to center
+    def _check_ball_speed(self):
+        # increment count upon collision with either paddle
+        self.paddle_collision_count += 1
+
+        # increase speed after collision with both paddles
+        if self.paddle_collision_count % 2 == 0:
+            self.ball.increase_speed()
+            self.paddle_collision_count = 0
+
+    # reposition ball to center after scoring
     def center_ball(self):
         self.ball.rect.centerx = WIDTH // 2
         self.ball.rect.centery = random.randint(BALL_HEIGHT + PADDLE_LIMIT, HEIGHT - BALL_HEIGHT - PADDLE_LIMIT)
@@ -106,6 +205,10 @@ class Table:
         self.ball.rect.centerx = WIDTH // 2
         self.ball.rect.centery = HEIGHT // 2
         self.ball.moving = False
+
+    def _reset_ball_speed(self):
+        self.paddle_collision_count = 0
+        self.ball.new_speed = BALL_SPEED
 
     # ball movement when game over
     def _reset_ball_movement(self):
@@ -143,7 +246,7 @@ class Table:
         # handle event for player win
         if self.winner is not None:
             self.game_over = True
-            declare = self.score_font.render(f"{self.winner} WIN!", False, 'white')
+            declare = self.text_font.render(f"{self.winner} WIN", False, 'white')
             self.win.blit(declare, (WIDTH//2 - declare.get_width()//2 + 30, HEIGHT//2 - declare.get_height()//2))
 
     # draw objects from table onto window
